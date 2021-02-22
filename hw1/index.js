c5ad30df7adf66e545db8b48ff04139f46724d1b
ceofil <gceofil@gmail.com>
2021-02-22 21:14:30 +0200

    basic router

--- hw1/index.js
+++ hw1/index.js
@@ -1,13 +1,55 @@
 var http = require("http");
 
+function handle_generate_exam(req, res) {
+  console.log("generating exam");
+}
+
+function handle_train_student(req, res) {
+  console.log("training student");
+}
+
+function handle_submit_results(req, res) {
+  console.log("results submitted");
+}
+
+let routes = [
+  {
+    url: "/api/generate_exam",
+    method: "GET",
+    handler: handle_generate_exam,
+  },
+  {
+    url: "/api/train_student",
+    method: "GET",
+    handler: handle_train_student,
+  },
+  {
+    url: "/api/submit_results",
+    method: "POST",
+    handler: handle_submit_results,
+  },
+];
+
 var server = http.createServer(function (req, res) {
-  if (req.url == "/") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "Hello World" }));
+  console.log(req.method, req.url);
+
+  var found = false;
+  for (var route of routes) {
+    if (route.url === req.url && route.method === req.method) {
+      route.handler(req, res);
+      found = true;
+      break;
+    }
+  }
+
+  if (found == false) {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.write(JSON.stringify({ message: "Invalid url/method" }));
     res.end();
   }
 });
 
-server.listen(5000);
+var port = 5000;
+server.listen(port);
 
-console.log("Node.js web server at port http://localhost:5000/ is running...");
+console.log(`Node.js web server at http://localhost:${port}/ is running...`);
---
+++ hw1/readme.md
@@ -0,0 +1,11 @@
+## generate exam
+
+https://pastebin.com/api/api_post.php
+
+## train student
+
+https://yesno.wtf/api
+
+## post results
+
+https://pastebin.com/api/api_post.php
